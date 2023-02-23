9f20ae30659ac3912e7a9221e5305b60050f6912
Jesriel Ledesma <91326451+uriel000@users.noreply.github.com>
2023-02-23 13:19:31 +0800

    Update delete_student.php

--- delete_student.php
+++ delete_student.php
@@ -1,62 +1,38 @@
 <?php
 require("connection.php");
 
-$students = mysqli_query($conn,"select * from users");
+$students = mysqli_query($conn,"select * from student");
 $j = mysqli_affected_rows($conn);
 
-echo "
-<table>
-<tr>
-    <th>ID</th>
-    <th>First</th>
-    <th>Middle</th>
-    <th>Last</th>
-    <th>Birthdate</th>
-    <th>Email</th>
-    <th>Student ID</th>
-    <th>Course</th>
+if(isset($_GET['id'])){
 
-</tr>";
-while($row = mysqli_fetch_assoc($students)){
+    $id = $_GET['id'];
     
-    $id = $row['id'];
-    $fname = $row["firstname"];
-    $mname = $row["middlename"];
-    $lname = $row["lastname"];
-    $studentid = $row["studentid"];
-    $email = $row["email"];
-    $birthdate = $row["birthdate"];
-    $course = $row["course"];
-
-    echo "
-        <tr>
-            <td>$id</td>
-            <td>$fname</td>
-            <td>$mname</td>
-            <td>$lname</td>
-            <td>$birthdate</td>
-            <td>$email</td>
-            <td>$studentid</td>
-            <td>$course</td>
-        </tr>
-    </table><br>";
-}
-
-echo "<form method=POST>
-<p><b>Remove</b> a student.</p>
-<label for=id>ID Number:</label><input type=text name=idnum id=id>
-<input type=submit name=btnDelete value='Delete Record'>
-</form>
-";
-
-if(isset($_POST['btnDelete'])){
-    $id = $_POST['idnum'];
-
-    $sql = "DELETE FROM users WHERE id = '$id'";
+    $sql = "DELETE FROM student WHERE student_id = '$id'";
     mysqli_query($conn,$sql);
-
-    echo "<br><b>Record has been deleted successfully!</b>";
-
     
-}
+    echo "<br><b>Record has been deleted successfully!</b>";
+    
+    header("Location: read_student.php");
+    exit;
+    }
 ?>
+
+<!-- 
+JAVASCRIPT DIALOG BOX TO CONFIRM DELETE
+
+<script>
+// JavaScript function to display confirmation dialog
+function confirmDelete(id) {
+    if (confirm("Are you sure you want to delete this record?")) {
+        // if user confirms, redirect to PHP script to delete the record
+        window.location.href = "delete_student.php?id=" + id;
+    }
+}
+</script>
+
+//The delete button must call the function
+echo "<td><a onClick=confirmDelete($id) href=student.php>Delete</a>
+    <a href=update_student.php?id=$id>Edit</a>
+</td>";
+ -->
